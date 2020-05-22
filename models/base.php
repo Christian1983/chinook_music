b65df61f09a17f5443200079fc75a24c1872af37
Christian Neumann <psychedelic.system@gmail.com>
2020-05-22 12:22:58 +0200

    meta programming for the win

--- models/base.php
+++ models/base.php
@@ -2,16 +2,44 @@
 class Base {
     private $db;
     private $table;
+    private $class_name;
     protected $properties = array();
 
     public function __construct($params) {
-        // params as key value
         $this->db = new SQLite3('db/chinook.db');
-        
+        $this->class_name = get_class($this);
+        $this->table = strtolower($this->class_namme) . 's';
+
         foreach($params as $key => $value) {
-            //echo "<p> $key => $value </p>";
             $this->properties[$key] = $value;
         }
+    }
+
+    public static function all() {
+        $sdb = new SQLite3('db/chinook.db');
+        $klass = get_called_class();
+        $table = strtolower(get_called_class()) . 's';
+        $res = $sdb->query("select * from $table;");
+
+        //instance array
+        $iarr=array();
+        $i=0;
+        while ($row = $res->fetchArray()) {
+            $iarr[$i] = new $klass(BASE::build_properties_from_array($row));
+            $i = $i + 1;
+        }
+        return $iarr;
+    }
+
+    public static function find($id) {
+        if (is_numeric($id))
+            $sdb = new SQLite3('db/chinook.db');
+            $klass = get_called_class();
+            $table = strtolower(get_called_class()) . 's';
+
+            $result = $db->exec("select * from tracks where TrackId = $id");
+            $row = $res->fetchArray();
+            return new $klass(BASE::build_properties_from_array($row));
     }
 
     public function __set($prop, $value) {
@@ -28,6 +56,19 @@ class Base {
             $tmp . "$key => $value";
         }
         return $tmp;
+    }
+
+
+    // private
+    private static function build_properties_from_array($array) {
+        $properties=array();
+        foreach($array as $key => $value) {
+            // ignore numerical keys
+            if( is_numeric($key) )
+                continue;
+            $properties[$key] = $value;
+        }
+        return $properties;
     }
 }
 ?>
--- models/track.php
+++ models/track.php
@@ -1,35 +1,6 @@
 <?php 
 class Track extends Base {
 
-    public static function all() {
-        $sdb = new SQLite3('db/chinook.db');
-        $res = $sdb->query('select * from tracks;');
-
-        //instance array
-        $iarr=array();
-        $i=0;
-        while ($row = $res->fetchArray()) {
-            $properties=array();
-            foreach($row as $key => $value) {
-                // ignore numerical keys
-                if( is_numeric($key) )
-                    continue;
-                $properties[$key] = $value;
-            }
-
-            $iarr[$i] = new Track($properties);
-            $i = $i + 1;
-        }
-        return $iarr;
-    }
-    
-
-    public static function find($id) {
-        if (is_numeric($id))
-            $result = $db->exec('select * from tracks where TrackId =' . $this->$id);
-            echo $result;
-    }
-
     public function album() {
         $db->exec('select * from albums where AlbumId =' . $this->album_id);
     }
@@ -37,5 +8,6 @@ class Track extends Base {
     public function artist() {
         $db->exec('select * from artists where ArtistId =' . $this->album_id);
     }
+
 }
 ?>
